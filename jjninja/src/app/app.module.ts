5fdb998e918f32454c0001069c79b31f2e442870
Janine Jay <janinerjay@Janines-MacBook-Pro.local>
2019-05-13 21:01:42 -0400

    removed homepage component

--- jjninja/src/app/app.module.ts
+++ jjninja/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomepageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
